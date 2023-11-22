42f7cbb2fda190fe8af2a3dc947fc9e9dcb07143
Xu-Gavin <g58xu@uwaterloo.ca>
2023-11-22 02:47:07 -0500

    Add intial Java files

---
+++ syncstream/src/test/java/com/syncstream/syncstream/SyncstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncstream.syncstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
