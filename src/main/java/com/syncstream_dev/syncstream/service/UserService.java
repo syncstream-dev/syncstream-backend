60ea43da75355d2b50b21508226fc409969b915a
Xu-Gavin <g58xu@uwaterloo.ca>
2023-12-24 18:54:46 -0600

    Implement register method

--- src/main/java/com/syncstream_dev/syncstream/service/UserService.java
+++ src/main/java/com/syncstream_dev/syncstream/service/UserService.java
@@ -1,9 +1,13 @@
 package com.syncstream_dev.syncstream.service;
 
+import com.syncstream_dev.syncstream.exception.DuplicateEmailException;
+import com.syncstream_dev.syncstream.exception.DuplicateUsernameException;
 import com.syncstream_dev.syncstream.model.User;
 import com.syncstream_dev.syncstream.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -15,11 +19,24 @@ public class UserService {
     }
 
     public User register(String username, String email, String password, String avatarUrl) throws Exception {
-        // Sample instantiation:
-        //  User john = new User("1", "John", "token", "test@example.com", "password", "avatarUrl");
-        // Check if user already exists and throw an exception if they do
-        // Save the user to the database
-        // Return the saved user
+        // Duplicate username and email check
+        if (userRepository.existsByUsername(username)) {
+            throw new DuplicateUsernameException("Username already exists");
+        } else if (userRepository.existsByEmail(email)) {
+            throw new DuplicateEmailException("Email already registered with an account");
+        }
+
+        // userId and token generation
+        UUID newUserId;
+        do {
+            newUserId = UUID.randomUUID();
+        } while (userRepository.existsById(newUserId));
+        String newToken = UUID.randomUUID().toString();
+
+        // Save and return newUser
+        User newUser = new User(newUserId, username, newToken, email, password, avatarUrl);
+        userRepository.save(newUser);
+        return newUser;
     }
 
     public User login(String username, String password) throws Exception {
